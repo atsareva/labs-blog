62f287ed511079511eb86ce2e4b9b9dab803e184
Alena Tsareva <alena.tsareva@webinse.com>
2013-04-19 23:56:00 +0300

    fixed converter

--- controllers/controller_convert.php
+++ controllers/controller_convert.php
@@ -1,30 +1,28 @@
 <?php
 
 include("assets/mpdf/mpdf.php");
-require_once 'model.php';
 
-class Controller_Convert extends Model
+class Controller_Convert
 {
 
-    function index()
+    function index($id = NULL)
     {
-        if (isset($_GET['id']) && $_GET['id'] > 0)
+        if (isset($id[0]))
         {
-            $where = array(
-                'id' => $_GET['id']
-            );
-            $material = $this->select($where, 'materials');
-            $html = $material['full_text'];
-            
-            $mpdf = new mPDF('utf-8', 'A4', '8', '', 10, 10, 7, 7, 10, 10); /* задаем формат, отступы и.т.д. */
-//$mpdf->charset_in = 'cp1251'; /*не забываем про русский*/
+            $material = Core::getModel('material')->load((int) $id[0]);
+            if ($material->getId())
+            {
+                $html = $material->getFullText();
 
-            $stylesheet = file_get_contents('assets/mpdf/style.css'); /* подключаем css */
-            $mpdf->WriteHTML($stylesheet, 1);
+                $mpdf = new mPDF('utf-8', 'A4', '8', '', 10, 10, 7, 7, 10, 10); /* задаем формат, отступы и.т.д. */
 
-            $mpdf->list_indent_first_level = 0;
-            $mpdf->WriteHTML($html, 2); /* формируем pdf */
-            $mpdf->Output('material.pdf', 'I');
+                $stylesheet = file_get_contents('assets/mpdf/style.css'); /* подключаем css */
+                $mpdf->WriteHTML($stylesheet, 1);
+
+                $mpdf->list_indent_first_level = 0;
+                $mpdf->WriteHTML($html, 2); /* формируем pdf */
+                $mpdf->Output($material->getTitle() . '.pdf', 'I');
+            }
         }
     }
 
