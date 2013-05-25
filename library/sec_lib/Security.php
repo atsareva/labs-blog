da04ea71212a5f454b56ddf13e84ce34aa5739b4
Alena Tsareva <alena.tsareva@webinse.com>
2013-05-25 17:02:01 +0300

    added function handler and logfile outputing

--- library/sec_lib/Security.php
+++ library/sec_lib/Security.php
@@ -17,15 +17,51 @@ class Security
         if (self::$_secDebug || self::$_securityCore->_secErrors)
         {
             error_reporting(E_USER_ERROR | E_USER_WARNING);
-            set_error_handler('seqErrorHandler');
+            set_error_handler('_seqErrorHandler');
         }
         else
             error_reporting(0);
     }
 
-    public static function echoSec()
+    private function _seqErrorHandler($code = '', $msg = '', $file = '', $line = '')
     {
-        echo self::$_secTokenName;
+        switch ($code)
+        {
+            case E_ERROR:
+                self::_secLog('Script Error', "line: $line script: $file error: $code reason: $msg");
+                break;
+            case E_WARNING:
+                self::_secLog('Script Warning', "line: $line script: $file error: $code reason: $msg");
+                break;
+            case E_NOTICE:
+                self::_secLog('Script Notice', "line: $line script: $file error: $code reason: $msg");
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Logfile output
+     */
+    private static function _secLog($message = '', $testName = '', $source = '')
+    {
+        if (self::$_securityCore->_secLog)
+        {
+            $rootdir = self::$_securityCore->_secBaseDir;
+            $logfile = fopen($rootdir . "seq_log/log.txt", "a");
+            fputs($logfile, date("d.m.Y, H:i:s", time()) .
+                    ", " . $_SERVER['REMOTE_ADDR'] .
+                    ", [" . $source . "]" .
+                    ", " . $message .
+                    ", " . $testName .
+                    ", " . $_SERVER['REQUEST_METHOD'] .
+                    ", " . $_SERVER['PHP_SELF'] .
+                    ", " . $_SERVER['HTTP_USER_AGENT'] .
+                    ", " . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '') .
+                    "\n");
+            fclose($logfile);
+        }
     }
 
 }
